5372026310982cf36a92e7e11655f7e501a8aeac
Clemens Koza <koza@pria.at>
2016-04-22 12:34:56 +0200

    correct messages module

--- hedgehog/protocol/messages.py
+++ hedgehog/protocol/messages.py
@@ -1,3 +1,4 @@
+import collections
 import hedgehog.protocol.proto.hedgehog_pb2
 
 
@@ -31,7 +32,40 @@ def AnalogUpdate(msg, sensors):
     msg.analog_update.sensors.update(sensors)
 
 
+AnalogState = collections.namedtuple('AnalogState', ('pullup',))
+
+
 @_message_init
-def AnalogPullupAction(msg, sensors):
+def AnalogStateAction(msg, sensors):
     """Creates a `HedgehogMessage` object representing an `AnalogPullupAction` command."""
-    msg.analog_pullup_action.sensors.update(sensors)
+    for port, state in sensors.items():
+        msg.analog_state_action.sensors[port].pullup = state.pullup
+
+
+@_message_init
+def DigitalRequest(msg, sensors):
+    """Creates a `HedgehogMessage` object representing an `DigitalRequest` command."""
+    msg.digital_request.sensors.extend(sensors)
+
+
+@_message_init
+def DigitalUpdate(msg, sensors):
+    """Creates a `HedgehogMessage` object representing an `DigitalUpdate` command."""
+    msg.digital_update.sensors.update(sensors)
+
+
+DigitalState = collections.namedtuple('DigitalState', ('pullup', 'output'))
+
+
+@_message_init
+def DigitalStateAction(msg, sensors):
+    """Creates a `HedgehogMessage` object representing an `DigitalStateAction` command."""
+    for port, state in sensors.items():
+        msg.digital_state_action.sensors[port].pullup = state.pullup
+        msg.digital_state_action.sensors[port].output = state.output
+
+
+@_message_init
+def DigitalAction(msg, sensors):
+    """Creates a `HedgehogMessage` object representing an `DigitalAction` command."""
+    msg.digital_action.sensors.update(sensors)
--- tests/test.py
+++ tests/test.py
@@ -2,7 +2,7 @@ import unittest
 from hedgehog.protocol import messages
 
 
-class TestProto(unittest.TestCase):
+class TestMessages(unittest.TestCase):
     def test_analog_request(self):
         msg = messages.AnalogRequest([0, 1])
         self.assertEqual(msg.WhichOneof('command'), 'analog_request')
@@ -12,6 +12,34 @@ class TestProto(unittest.TestCase):
         msg = messages.AnalogUpdate({0: 2, 1: 1000})
         self.assertEqual(msg.WhichOneof('command'), 'analog_update')
         self.assertEqual(msg.analog_update.sensors, {0: 2, 1: 1000})
+
+    def test_analog_state_action(self):
+        msg = messages.AnalogStateAction({0: messages.AnalogState(True)})
+        self.assertEqual(msg.WhichOneof('command'), 'analog_state_action')
+        self.assertEqual(len(msg.analog_state_action.sensors), 1)
+        self.assertEqual(msg.analog_state_action.sensors.get(0).pullup, True)
+
+    def test_digital_request(self):
+        msg = messages.DigitalRequest([0, 1])
+        self.assertEqual(msg.WhichOneof('command'), 'digital_request')
+        self.assertEqual(msg.digital_request.sensors, [0, 1])
+
+    def test_digital_update(self):
+        msg = messages.DigitalUpdate({0: True, 1: False})
+        self.assertEqual(msg.WhichOneof('command'), 'digital_update')
+        self.assertEqual(msg.digital_update.sensors, {0: True, 1: False})
+
+    def test_digital_state_action(self):
+        msg = messages.DigitalStateAction({0: messages.DigitalState(True, False)})
+        self.assertEqual(msg.WhichOneof('command'), 'digital_state_action')
+        self.assertEqual(len(msg.digital_state_action.sensors), 1)
+        self.assertEqual(msg.digital_state_action.sensors.get(0).pullup, True)
+        self.assertEqual(msg.digital_state_action.sensors.get(0).output, False)
+
+    def test_digital_action(self):
+        msg = messages.DigitalAction({0: True, 1: False})
+        self.assertEqual(msg.WhichOneof('command'), 'digital_action')
+        self.assertEqual(msg.digital_action.sensors, {0: True, 1: False})
 
 
 if __name__ == '__main__':
