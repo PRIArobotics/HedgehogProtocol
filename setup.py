02340cb68aa301b50f39242f62d1b10c54076fd0
SillyFreak <sillyfreak.ck@gmail.com>
2016-09-25 18:48:06 +0200

    add missing hedgehog-utils dependency, pull in protobuf and pyzmq via extras

--- setup.py
+++ setup.py
@@ -46,7 +46,7 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['protobuf>=3.0.*', 'pyzmq'],
+    install_requires=['hedgehog-utils[protobuf,zmq]'],
 
     # You can install these using the following syntax, for example:
     # $ pip install -e .[dev,test]
