93f7cefc52ef8adfa5bd00ad29e39a1320fbfbc8
SillyFreak <sillyfreak.ck@gmail.com>
2017-06-15 19:55:09 +0200

    first code generator version

--- tasks.py
+++ tasks.py
@@ -4,3 +4,8 @@ from invoke import run, task
 @task
 def protoc(context):
     run("protoc --proto_path=proto --python_out=. `find proto -name '*.proto'`")
+
+
+@task
+def gsl(context):
+    run("gsl gsl/protocol.xml")
