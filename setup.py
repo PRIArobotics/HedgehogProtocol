b9cf13411b420823e937477e0ea6317bed1818a3
Clemens Koza <koza@pria.at>
2019-09-25 15:45:14 +0200

    get rid of dummy types, use distinct message types everywhere

--- setup.py
+++ setup.py
@@ -51,7 +51,7 @@ setup(
     # You can install these using the following syntax, for example:
     # $ pip install -e .[dev,test]
     extras_require={
-        'dev': ['invoke', 'hedgehog-protocol-spec ~=0.9.0rc1',
+        'dev': ['invoke', 'hedgehog-protocol-spec ~=0.9.0',
                 'pytest', 'pytest-runner', 'pytest-asyncio', 'pytest-trio', 'pytest-cov', 'pytest-timeout', 'mypy'],
     },
 
